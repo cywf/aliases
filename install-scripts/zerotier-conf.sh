a851ce3c2e063f8543e334bb78558dc9f3ce898d
cywf <kyle.parisher@owasp.org>
2023-01-21 12:46:14 -0400

    updated readme, added zerotier conf install script (todo)

---
+++ install-scripts/zerotier-conf.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## ZeroTier configuration script
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+## Install ZeroTier on your gateway, join your network and authorize it, and then configure it as a basic IPv4 NAT router
+
